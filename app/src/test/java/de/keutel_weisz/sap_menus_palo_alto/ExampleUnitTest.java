ef603618a3bf027b82df107fab2481f6e76bd516
Jonas Keutel <jonas@keutel.de>
2015-10-20 19:18:20 -0700

    initial commit

---
+++ app/src/test/java/de/keutel_weisz/sap_menus_palo_alto/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package de.keutel_weisz.sap_menus_palo_alto;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
