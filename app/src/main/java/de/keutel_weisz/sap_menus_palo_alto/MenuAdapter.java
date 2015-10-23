a0d9cb312bdac676fd8d6f4bc2f9cea09ffe1ac0
Jonas Keutel <jonas@keutel.de>
2015-10-22 17:10:45 -0700

    Implements basic ListView and StringAdapter

---
+++ app/src/main/java/de/keutel_weisz/sap_menus_palo_alto/MenuAdapter.java
@@ -0,0 +1,32 @@
+package de.keutel_weisz.sap_menus_palo_alto;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by jonas on 22.10.2015.
+ */
+public class MenuAdapter extends ArrayAdapter<String> {
+
+    private final Context context;
+    private final String[] values;
+
+    public MenuAdapter(Context context, String[] values) {
+        super(context, -1, values);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View menuEntryView = inflater.inflate(R.layout.fragment_main_menu_entry, parent, false);
+        TextView menuHeadingView = (TextView) menuEntryView.findViewById(R.id.fragment_main_tv_menu_heading);
+        menuHeadingView.setText(values[position]);
+        return menuEntryView;
+    }
+}
